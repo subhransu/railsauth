81427bc4de419f6614171cd22ea1b206af415c46
Subhransu Beher <subh@subhb.org>
2014-08-28 21:56:52 +0800

    added migration for photo upload

--- db/migrate/20140828132607_create_photos.rb
+++ db/migrate/20140828132607_create_photos.rb
@@ -1,7 +1,10 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
-
+      t.string :name
+      t.string :title
+      t.string :image_url
+      t.integer :user_id      
       t.timestamps
     end
   end
