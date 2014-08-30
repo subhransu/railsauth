7cf507ead88b4388133e2e1e6968701b22c4ece4
subhransu <subh@subhb.org>
2014-08-30 18:18:41 +0800

    some changes in api controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -15,7 +15,7 @@ class ApiController < ApplicationController
         user = User.new(user_params)
 
         if user.save
-          render :json => @user.to_json, :status => 200
+          render :json => user.to_json, :status => 200
         else
           error_str = ""
 
